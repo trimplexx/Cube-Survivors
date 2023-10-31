11e9c901ff27dc979e10fb5243422024f0618db1
Maffias <mateuszmatwiej@wp.pl>
2023-10-31 17:23:38 +0100

    Character movement and rotation
    Character assets, scripts for movement of character, rotation of character (based on mouse position), and camera follow character script.

---
+++ Assets/Character/Scripts/Camera_Follow_Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Follow_Player : MonoBehaviour
+{
+
+    public GameObject character; //zmienna do której wrzucamy postać, za którą podążą kamera
+
+    public float cameraHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraHeight = 22;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = character.transform.position + new Vector3(0, cameraHeight, -cameraHeight/(1.7f));
+    }
+}
---
+++ Assets/Character/Scripts/Player_Movement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    public float horizontalinput;
+    public float verticalinput;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 3.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Read values from keyboard
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Create a direction vector based on the input
+        Vector3 moveDirection = new Vector3(horizontalInput, 0.0f, verticalInput).normalized;
+
+        // Rotate towards the mouse cursor
+        RotateTowardsMouseCursor();
+
+        // Move the object in the direction of the input
+        transform.Translate(moveDirection * speed * Time.deltaTime, Space.World); //Space.World is used so the character moves in the world, and not based on it's rotation
+    }
+
+    void RotateTowardsMouseCursor()
+    {
+        // Create a ray from the camera to the mouse cursor position
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // Perform a raycast to find the point on the ground
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            Vector3 targetPosition = hit.point;
+            targetPosition.y = transform.position.y; // Keep the same y-position to avoid tilting
+
+            // Rotate the character to look at the target position
+            transform.LookAt(targetPosition);
+        }
+    }
+}
